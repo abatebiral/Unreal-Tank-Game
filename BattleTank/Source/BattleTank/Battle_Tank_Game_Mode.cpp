2fb6783b451164bc3c464d55e15aa41e29fab3d6
André Biral <andre.biral@icloud.com>
2017-10-11 10:31:26 -0300

    New Game Mode created.

---
+++ BattleTank/Source/BattleTank/Battle_Tank_Game_Mode.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Battle_Tank_Game_Mode.h"
+
+
+
+
---
+++ BattleTank/Source/BattleTank/Battle_Tank_Game_Mode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Battle_Tank_Game_Mode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ABattle_Tank_Game_Mode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
